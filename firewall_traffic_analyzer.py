a7ddd2c7327044e5966f95819196f85e06ad5756
William R. Storrs <wstorrs@fingerlakes.edu>
2026-02-16 05:31:09 +0000

    added code for the Firewall Traffic Analyzer lab, tested with sample outputs with success. Answered reflective question

--- firewall_traffic_analyzer.py
+++ firewall_traffic_analyzer.py
@@ -1,8 +1,8 @@
 # FILE NAME - firewall_traffic_analyzer.py
 
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+# NAME: William Storrs
+# DATE: February 16, 2026
+# BRIEF DESCRIPTION:  Firewall Traffic Analyzer
 
 
 
@@ -15,12 +15,33 @@
 
 ########## ENTER YER CODE BELOW THIS LINE ##########
 
+print("=== Network Traffic Security Analyzer ===\n")
 
+# Inputs
+port = int(input("Enter the port number (e.g., 80, 22, 443, 3389): "))
+size = float(input("Enter the data transfer size in megabytes (MB): "))
 
+# Risk Logic
+# Assumptions
+# - 80 = HTTP (unencrypted)
+# - 443 = HTTPS (encrypted)
+# - 22 = SSH (generally encrypted)
+# - 3389 = RDP (can be risk if exposed)
 
+if port == 80 and size >= 100:
+    risk = "MEDIUM RISK: Large unencrypted data transfer detected."
+elif port == 22 or port == 3389 and size >= 100:
+    risk = "HIGH RISK: Potential unauthorized remote access detected!"
+elif port == 443:
+    risk = "LOW RISK: Secure encrypted transfer detected."
+else:
+    risk = "UNKNOWN: Unrecognized traffic pattern."
 
-
-
+# Output
+print("\nFIREWALL LOG:")
+print(f"Port: {port}, Transfer Size: {size:g} MB")
+print(f"Risk Assessment: {risk}")
+print("-" * 25)
 
 
 
@@ -50,10 +71,10 @@ Risk Assessment: MEDIUM RISK: Large unencrypted data transfer detected.
 === Network Traffic Security Analyzer ===
 
 Enter the port number (e.g., 80, 22, 443, 3389): 22
-Enter the data transfer size in megabytes (MB): 12
+Enter the data transfer size in megabytes (MB): 1200
 
 FIREWALL LOG:
-Port: 22, Transfer Size: 12 MB
+Port: 22, Transfer Size: 1200 MB
 Risk Assessment: HIGH RISK: Potential unauthorized remote access detected!
 ------------------------
 '''
@@ -90,10 +111,7 @@ Risk Assessment: UNKNOWN: Unrecognized traffic pattern.
 
 1. Did you get tripped up using the `or` or `and` operators? If so, how?
 
-
-
-
-
+I did not. 
 
 
 '''
