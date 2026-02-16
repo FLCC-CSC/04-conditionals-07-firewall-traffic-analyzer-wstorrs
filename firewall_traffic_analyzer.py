067bc35d7bfc92c56a5f55ee3190979ba9ca53d5
William R. Storrs <wstorrs@fingerlakes.edu>
2026-02-16 05:42:54 +0000

    added parenthesis around high risk ports as suggested by autograde error analyzer

--- firewall_traffic_analyzer.py
+++ firewall_traffic_analyzer.py
@@ -30,7 +30,7 @@ size = float(input("Enter the data transfer size in megabytes (MB): "))
 
 if port == 80 and size >= 100:
     risk = "MEDIUM RISK: Large unencrypted data transfer detected."
-elif port == 22 or port == 3389 and size >= 100:
+elif (port == 22 or port == 3389) and size >= 100:
     risk = "HIGH RISK: Potential unauthorized remote access detected!"
 elif port == 443:
     risk = "LOW RISK: Secure encrypted transfer detected."
